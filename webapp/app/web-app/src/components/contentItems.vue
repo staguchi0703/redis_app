28f1369e3741af9a8542e36250ab0542a90f9a4a
Sho Taguchi <s.taguchi703@gmail.com>
2022-09-04 17:57:19 +0900

    0001

---
+++ webapp/app/web-app/src/components/contentItems.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    methods : {
+        get_val () {
+            this.key001 = "aaa"
+        },
+    }
+})
+
+
+</script>
+
+<template>
+    <div class="bg-light">
+        this is contents
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
